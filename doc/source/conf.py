af033e530abb39ac74e1c835365b358ae895e606
Nino <ninomyemail@gmail.com>
2020-03-05 00:02:10 +0800

    Update conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -50,3 +50,7 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# solve Sphinx error:
+# master file /home/docs/checkouts/readthedocs.org/user_builds/nino-hist/checkouts/latest/doc/source/contents.rst not found
+master_doc = 'index'
